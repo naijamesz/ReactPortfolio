adbf32751dace13fc42323cac37022d18e03d794
naijamesz <kodlikejamesz@hotmail.com>
2024-02-05 17:01:26 +0700

    Added portfolio projects.

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,9 +1,14 @@
 import self from '../img/self.png';
-import mock1 from '../img/mock1.png';
-import mock2 from '../img/mock2.png';
-import mock3 from '../img/mock3.png';
-import mock4 from '../img/mock4.png';
-import mock5 from '../img/mock5.png';
+import mock1 from '../img/mock1-.png';
+import mock2 from '../img/mock2-.png';
+import mock3 from '../img/mock3-.png';
+import mock4 from '../img/mock4-.png';
+import mock5 from '../img/mock5-.png';
+import mock6 from '../img/mock6-.png';
+import mock7 from '../img/mock7-.png';
+import mock8 from '../img/mock8-.png';
+import mock9 from '../img/mock9-.png';
+import mock10 from '../img/mock10-.png';
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -33,15 +38,15 @@ export const info = {
     // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
     {
       emoji: '🌎',
-      text: 'Based in Phuket / Thailand.',
+      text: 'Based in Phuket / Thailand.'
     },
     {
       emoji: '☕',
-      text: 'Dev and Music Lover.',
+      text: 'Dev and Music Lover.'
     },
     {
       emoji: '📧',
-      text: 'james_web.dev@outlook.com',
+      text: 'james_web.dev@outlook.com'
       // text: 'jamesz_duckveloper@hotmail.com',
     },
     {
@@ -55,35 +60,35 @@ export const info = {
         <a href='https://www.frontendmentor.io/profile/NaiJamesz'>
           <strong>Frontend Mentor.</strong>
         </a>
-      ),
-    },
+      )
+    }
   ],
   socials: [
     {
       link: 'https://facebook.com/naijamesz',
       icon: 'fa fa-facebook',
-      label: 'facebook',
+      label: 'facebook'
     },
     {
       link: 'https://instagram.com/naijamesz',
       icon: 'fa fa-instagram',
-      label: 'instagram',
+      label: 'instagram'
     },
     {
       link: 'https://github.com/naijamesz',
       icon: 'fa fa-github',
-      label: 'github',
+      label: 'github'
     },
     {
       link: 'https://linkedin.com/in/thanawat-sanguanporn-373107198',
       icon: 'fa fa-linkedin',
-      label: 'linkedin',
+      label: 'linkedin'
     },
     {
       link: 'https://twitter.com/nai_jameszix',
       icon: 'fa fa-twitter',
-      label: 'twitter',
-    },
+      label: 'twitter'
+    }
 
     // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
     // Just change the links so that they lead to your social profiles.
@@ -108,29 +113,29 @@ export const info = {
       'bulma',
       'next',
       'express',
-      'mongodb',
+      'mongodb'
     ],
 
-    tools: ['nodejs', 'netlify', 'vscode', 'vercel', 'figma', 'Postman'],
+    tools: ['nodejs', 'netlify', 'vscode', 'vercel', 'figma', 'Postman']
   },
 
   hobbies: [
     {
       label: 'reading',
-      emoji: '📖',
+      emoji: '📖'
     },
     {
       label: 'coding',
-      emoji: '💻',
+      emoji: '💻'
     },
     {
       label: 'listening to music',
-      emoji: '🎧',
+      emoji: '🎧'
     },
     {
       label: 'exploring new technologies',
-      emoji: '🔭',
-    },
+      emoji: '🔭'
+    }
     // Same as above, change the emojis to match / relate to your hobbies or interests.
     // You can also remove the emojis if you'd like, I just think they look cute :P
   ],
@@ -138,58 +143,63 @@ export const info = {
     // This is where your portfolio projects will be detailed
     {
       title: 'Project 1',
-      live: 'https://paytonpierce.dev', //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-      source: 'https://github.com/paytonjewell', // this should be a link to the **repository** of the project, where the code is hosted.
-      image: mock1,
+      live: 'https://naijamesz-portfolio-port.vercel.app/', //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+      source: 'https://github.com/naijamesz/ReactPortfolio', // this should be a link to the **repository** of the project, where the code is hosted.
+      image: mock1
     },
     {
       title: 'Project 2',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock2,
+      live: 'https://word-analytics-six.vercel.app',
+      source: 'https://github.com/naijamesz/word-analytics',
+      image: mock2
     },
     {
       title: 'Project 3',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock3,
+      live: 'https://best-seller-book-react.vercel.app',
+      source: 'https://github.com/naijamesz/Best-seller-book-react',
+      image: mock3
     },
     {
       title: 'Project 4',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock4,
+      live: 'https://github.com/naijamesz/travel-site',
+      source: 'https://travel-site-naijamesz.netlify.app',
+      image: mock4
     },
     {
       title: 'Project 5',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock5,
+      live: 'https://react-tailwind-shoes-shop.vercel.app',
+      source: 'https://github.com/naijamesz/react-tailwind-shoes-shop',
+      image: mock5
     },
     {
       title: 'Project 6',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock5,
+      live: 'https://ecommerce-product-page-for-fem.vercel.app',
+      source: 'https://github.com/naijamesz/FEM-ecommerce-product-page',
+      image: mock6
     },
     {
       title: 'Project 7',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock5,
+      live: 'https://reading-list-client-crud.vercel.app',
+      source: 'https://github.com/naijamesz/reading-list-client-crud',
+      image: mock7
     },
     {
       title: 'Project 8',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock5,
+      live: 'https://fem-testimonial-tailwind-css-nuuu.vercel.app',
+      source: 'https://github.com/naijamesz/FEM-testimonial-tailwind-css',
+      image: mock8
     },
     {
       title: 'Project 9',
-      live: 'https://paytonpierce.dev',
-      source: 'https://github.com/paytonjewell',
-      image: mock5,
+      live: 'https://naijamesz.github.io/analog-clocks/',
+      source: 'https://github.com/naijamesz/analog-clocksl',
+      image: mock9
     },
-  ],
+    {
+      title: 'Project 10',
+      live: 'https://github.com/naijamesz/React-BMI-calc-oldproject',
+      source: 'https://react-bmi-calc-oldproject.vercel.app',
+      image: mock10
+    }
+  ]
 };
-
